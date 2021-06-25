ece4c489519172273c1c10c429bd7f7a4ec30509
gunslingerOP <yahyahassan7219@yahoo.com>
2021-06-25 20:00:12 +0300

    modifications

--- src/handlers/get-all-items.js
+++ src/handlers/get-all-items.js
@@ -6,20 +6,22 @@ const dynamodb = require("aws-sdk/clients/dynamodb");
 const docClient = new dynamodb.DocumentClient();
 
 // Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
 exports.getAllItemsHandler = async (event) => {
-  const { httpMethod, path } = event;
+  const { body, httpMethod, path } = event;
   if (httpMethod !== "GET") {
     throw new Error(
       `getAllItems only accept GET method, you tried: ${httpMethod}`
     );
   }
   console.log("Works");
-  const response = `It works`;
-
+  response = {
+    statusCode: 200,
+    body,
+    headers: { "Content-Type": "application/json" },
+  };
   return response;
 };
